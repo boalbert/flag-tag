0cd436744fa67806b45586be12f4ab58c3ac90f5
boalbert <andersson.albert@gmail.com>
2021-05-12 13:52:12 +0200

    Added 'You are logged in as' as a component to Profile-page, fetches data from localStorage

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="myprofile">
 		<h1>My profile</h1>
+		<ProfileUserInfo />
 		<h2>Change username</h2>
 
 		<h2>My stats</h2>
@@ -11,11 +12,13 @@
 </template>
 <script>
 import ProfileSettings from '@/components/ProfileSettings.vue'
+import ProfileUserInfo from '@/components/ProfileUserInfo.vue'
 
 export default {
 	name: 'ProfilePage',
 	components: {
 		ProfileSettings,
+		ProfileUserInfo,
 	},
 	methods: {
 		logOut() {
