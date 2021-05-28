a7b46776a265fe816fe04d1b40fc87f2e5f5b7e3
Helena <helenalundstrom@hotmail.se>
2021-05-28 10:10:23 +0200

    Added so that you can reset one players highscore.. Simple css styling to the component and message if all goes well

--- src/components/ProfileAdmin.vue
+++ src/components/ProfileAdmin.vue
@@ -1,28 +1,48 @@
 <template>
-<div>
-  <button @click="resetClick= !resetClick" class="change-button">Reset Highscore All regions</button>
-  <div v-if="resetClick">
+<div class="admin-container">
+  <div class="resetAllHighscores">
+  <button @click="resetClick= !resetClick" class="menu-button">Reset Highscore All regions</button>
+  <div v-if="resetClick" class="resetAll">
     <p v-if="!resetSuccess">Are you sure?</p>
     <p v-if="resetSuccess">Reset successful</p>
-    <button @click="resetAllRegions" class="change-button">Yes!</button>
-    <button @click="resetClick = false" class="change-button">Cancel</button>
+    <button @click="resetAllRegions">Yes!</button>
+    <button @click="resetClick = false" class="cancel-resetAll-button">Cancel</button>
   </div>
-  <button @click="deleteClick= !deleteClick" class="change-button">Delete an account</button>
-  <div v-if="deleteClick" class="changeName-form">
+  </div>
+  <div class="resetOneHighscore">
+  <button @click="resetOneCheck" class="menu-button">Reset one players highscore</button>
+  <div v-if="resetOne" class="resetOne-form">
+    <p v-if="resetOneSuccess">Reset succesful</p>
+    <form @submit.prevent="resetOneAccount(userNameToReset)">
+      <input
+          required
+          type="text"
+          placeholder="Enter username to reset"
+          name="userNameToReset"
+          v-model.trim="userNameToReset"
+      />
+      <button type="submit" > Reset players highscore </button>
+    </form>
+    <button @click="resetOneCheck" class="cancel-reset-button">Cancel</button>
+  </div>
+  </div>
+  <div class="deleteOneAccount">
+  <button @click="deleteAccountCheck" class="menu-button">Delete an account</button>
+  <div v-if="deleteClick" class="deleteAccount-form">
+    <p v-if="deleteSuccess">Account deleted</p>
     <form @submit.prevent="deleteAccount(userNameToDelete)">
       <input
           required
           type="text"
-          placeholder="Who do you want to delete?"
+          placeholder="Enter username to delete"
           name="userNameToDelete"
           v-model.trim="userNameToDelete"
       />
-      <button type="submit">
-        Delete Account
-      </button>
+      <button type="submit"> Delete Account </button>
     </form>
+    <button @click="deleteAccountCheck" class="cancel-delete-button">Cancel</button>
   </div>
-
+  </div>
 </div>
 </template>
 
@@ -34,19 +54,32 @@ name: "ProfileAdmin",
     return {
       resetClick: false,
       resetSuccess: false,
+      resetOneSuccess: false,
+      deleteSuccess: false,
       deleteClick: false,
-      userNameToDelete: ''
+      resetOne: false,
+      userNameToDelete: '',
+      userNameToReset: ''
     }
   },
 
   methods: {
-      resetAllRegions(){
+  resetOneCheck(){
+    this.resetOne= !this.resetOne
+    if(!this.resetOne){
+      this.resetOneSuccess = false;
+    }
+  },
+    deleteAccountCheck(){
+      this.deleteClick= !this.deleteClick
+      if(!this.deleteClick){
+        this.deleteSuccess = false;
+      }
+    },
+    resetAllRegions(){
 
         fetch('http://localhost:3000/highscore', {
           method: 'PUT',
-          headers: {
-            'Content-type': 'application/json',
-          },
 
         })
             .then((response) => response.json())
@@ -63,9 +96,6 @@ name: "ProfileAdmin",
       fetch(
           `http://localhost:3000/users/${usernameToDelete}`, {
         method: 'DELETE',
-        headers: {
-          'Content-type': 'application/json',
-        },
       })
           .then((response) => response.json())
           .then((data) => {
@@ -73,13 +103,74 @@ name: "ProfileAdmin",
               console.log(data.errors)
             } else if (data.success) {
               console.log("Account deleted")
+              this.userNameToDelete = ''
+              this.deleteSuccess = true
             }
           })
-    }
+    },
+    resetOneAccount(usernameToReset){
+      fetch(`http://localhost:3000/highscore/${usernameToReset}`, {
+        method: 'PUT',
+
+      })
+          .then((response) => response.json())
+          .then((data) => {
+            if (!data.success) {
+              console.log(data.errors)
+            } else if (data.success) {
+              console.log("Reset successful")
+              this.userNameToReset = ''
+              this.resetOneSuccess = true
+            }
+          })
+    },
   }
 }
 </script>
 
 <style scoped>
+
+.admin-container{
+ display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+
+button{
+  padding: 8px;
+  margin: 5px;
+}
+
+.cancel-reset-button{
+  margin-top: -5px;
+  margin-bottom: 20px;
+  width: 165px;
+}
+
+.cancel-resetAll-button{
+  margin-bottom: 20px;
+}
+
+.cancel-delete-button{
+  margin-top: -5px;
+  width: 105px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+}
+
+input {
+  padding: 10px;
+  margin-bottom: 5px;
+}
+
+.menu-button {
+  width: 250px;
+}
 
 </style>
